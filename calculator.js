1104b9ae56bf00ead12f1eb7112e230a5c3b456d
neo123 <218580862+Varembill@users.noreply.github.com>
2025-11-12 16:57:00 +0700

    streamline the code

--- calculator.js
+++ calculator.js
@@ -2,20 +2,22 @@
 
 // const resutl;
 let calculation = [];
+let result2 = [];
 let start = 0.0;
 const digit = document.querySelector(".digit-buttons");
-const screen = document.querySelector(".calculator-screen")
-const reset = document.querySelector(".reset-button")
-
+const screen = document.querySelector(".calculator-screen");
+const reset = document.querySelector(".reset-button");
+const decimal = document.querySelector(".decimal-button");
+const operator =document.querySelector(".operation-button");
 
   digit.addEventListener("click", (e) => {
     const btn = e.target.closest('button');
-    const result2 = calculation.join("");
     if (btn) {
-      calculation.push(parseFloat(btn.textContent));
-      console.log(calculation);
-      console.log(result2);
-    
+      calculation.push(btn.textContent);
+      const result = calculation.join("");
+      console.log(result);
+      screen.textContent = result;
+
     }
   });
 
@@ -24,23 +26,10 @@ const reset = document.querySelector(".reset-button")
     // displayResult();
   })
 
-// function displayResult() {
-//   screen.textContent = calculation.toString() ;
-// }
-function add() {}
-function subtract() {
-
-}
-function multiply() {}
-function subtract() {}
-
 function operate(a,operator,b) {
+
+
 }
 
 
 
-    
-//pseudo code:
-/*Create the functions that populate the display when you click the digit buttons. 
-You should store the content 
-of the display (the number) in a variable for use in the next step.*/
