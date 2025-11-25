c4670114d09919f7273b1d9995bc358bb6395476
neo123 <218580862+Varembill@users.noreply.github.com>
2025-11-25 17:15:24 +0700

    created logic to calculate number

--- calculator.js
+++ calculator.js
@@ -1,43 +1,61 @@
 
 
 // const resutl;
-let calculation = [];
+let currentValue = '';
+let finalResult = 0;
 let result2 = [];
 let start = 0.0;
+let start2 = []
 const digit = document.querySelector(".digit-buttons");
 const screen = document.querySelector(".calculator-screen");
 const reset = document.querySelector(".reset-button");
 const decimal = document.querySelector(".decimal-button");
-const operator =document.querySelector(".operation-button");
-
-
-
-
+const operators = document.querySelector(".operation-button");
 
   digit.addEventListener("click",enterNumber );
   digit.addEventListener("keydown", enterNumber);
+  operators.addEventListener("click",operate)
 
 function enterNumber(e) {
   const btn = e.target.closest('button');
     if (btn) {
-      calculation.push(btn.textContent);
-      const result = calculation.join("");
-      screen.textContent = result;
+      currentValue += btn.textContent;
+      displayResult();
+      //logging the currentValue
+      // console.log(currentValue);
 
     }
 
 }
 
+function putItIn () {
+
+}
 
   reset.addEventListener("click", () =>{
-    calculation.length = 0;
-    // displayResult();
+    currentValue = '';
+    finalResult = 0;
+    displayResult();
   })
 
-function operate(a,operator,b) {
+function addNumber (a,b) {
+    return a + b ;
 
+}
+
+function operate(element) {
+  const btn2 = element.target.closest('button').textContent;
+  if (btn2 === "+") {
+    result2.push(currentValue);
+    finalResult += +currentValue;
+    currentValue = '';
+    console.log(finalResult);
+    
+  }
 
 }
 
 
-
+function displayResult () {
+  screen.textContent = currentValue;
+}
